88534b1014564ae2ea3bf603734fd0ba86a4e57d
unknown <jw362j@.ITServices.sbc.com>
2017-01-07 12:52:49 -0500

    add files for new project

---
+++ AndroidProject_Root/GoogleDriveTest/src/main/java/com/file/googledrive/util/FileUtil.java
@@ -0,0 +1,65 @@
+package com.file.googledrive.util;
+
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import com.google.api.client.http.FileContent;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by jw362j on 1/7/2017.
+ */
+public class FileUtil {
+
+    public static void FileList(Drive service) throws IOException {
+        // Print the names and IDs for up to 10 files.
+        FileList result = service.files().list()
+                .setPageSize(10)
+                .setFields("nextPageToken, files(id, name)")
+                .execute();
+        List<File> files = result.getFiles();
+        if (files == null || files.size() == 0) {
+            System.out.println("No files found.");
+        } else {
+            System.out.println("Files:");
+            for (File file : files) {
+                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+            }
+        }
+    }
+/*
+    public static File createFile(Drive service, String title, String description,
+                                  String parentId, String mimeType, String filename) {
+        // File's metadata.
+        File body = new File();
+        body.setTitle(title);
+        body.setDescription(description);
+        body.setMimeType(mimeType);
+
+        // Set the parent folder.
+        if (parentId != null && parentId.length() > 0) {
+            body.setParents(
+                    Arrays.asList(new ParentReference().setId(parentId)));
+        }
+
+        // File's content.
+        java.io.File fileContent = new java.io.File(filename);
+        FileContent mediaContent = new FileContent(mimeType, fileContent);
+        try {
+            File file = service.files().insert(body, mediaContent).execute();
+
+            // Uncomment the following line to print the File ID.
+            // System.out.println("File ID: " + file.getId());
+
+            return file;
+        } catch (IOException e) {
+            System.out.println("An error occured: " + e);
+            return null;
+        }
+
+    }*/
+}
+
