34272cdc5d4e343fb2f61d4b9563cfffe8e57fac
jiunian wang <jw362j@att.com>
2018-05-31 19:17:02 -0400

    delete files...

--- AndroidProject_Root/MultiCamera/make.bat
+++ AndroidProject_Root/MultiCamera/make.bat
@@ -40,7 +40,7 @@ adb shell am start -a android.intent.action.MAIN -c android.intent.category.LAUN
 goto end
 
 :mylog
-adb pull /sdcard/mylog/log_holder.log
+adb pull /sdcard/mylog/ ../
 goto end
 
 
--- AndroidProject_Root/make.bat
+++ AndroidProject_Root/make.bat
@@ -1,33 +1,20 @@
 @echo off
 doskey ls=dir $*
 set MYHOME=C:\Users\jw362j\a\z_temp\MultiCamera\MultiCamera\AndroidProject_Root\
-if  "%1" == "" goto end
-if  %1 == 1 goto one
-if  %1 == 2 goto two
-if  %1 == 3 goto three
-if  %1 == log goto logs
+if  "%1" == "" goto mymake
+if  %1 == l goto logs
 
 
 
 rem clean the project and take the build preparation
-:one
-cd MultiCamera
+:mymake
+echo "gradle build"
+gradle build
 goto end
-
-rem make and build the jar for sdk lib project
-:two
-cd MyAlarm
-goto end
-
-rem install the new builded jar into the destination directory
-:three
-cd MyToolLib
-goto end
-
 
 :logs
 del log_sdk_lib.txt
-adb pull /sdcard/tguard/log_sdk_lib.txt  ./log_sdk_lib.txt
+adb pull /sdcard/mylog/  .
 goto end
 
 
